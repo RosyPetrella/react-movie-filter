e5aa4d4f2c09cd23e01e3060f5f5a085f26f2b3d
RosyPetrella <petrellamr@gmail.com>
2025-03-27 16:54:30 +0100

    Add genre filtering in App component and enhance styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,32 @@ export default function App() {
   const [selectedGenre, setSelectedGenre] = useState("");
   const [filteredMovies, setFilteredMovies] = useState(movies);
 
+  useEffect(() => {
+    if (selectedGenre === "") {
+      setFilteredMovies(movies);
+    } else {
+      const filtered = movies.filter((movie) => movie.genre === selectedGenre);
+      setFilteredMovies(filtered);
+    }
+  }, [selectedGenre]);
+
+  const genres = [...new Set(movies.map((movie) => movie.genre))];
+
   return (
     <>
       <Header />
-      <Card />
+      <select
+        value={selectedGenre}
+        onChange={(e) => setSelectedGenre(e.target.value)}
+      >
+        <option value="">All genres</option>
+        {genres.map((genre, index) => (
+          <option key={index} value={genre}>
+            {genre}
+          </option>
+        ))}
+      </select>
+      <Card movies={filteredMovies} />
     </>
   );
 }
